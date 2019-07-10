6a7f471dda499830697c24f7b2f269cd14a3d563
Sergey Govyazin <SGV123@yandex.ru>
2019-07-10 17:59:33 +0300

    Updated to last 2.1.0 version of common, added default profile

--- src/HayaModule.ts
+++ src/HayaModule.ts
@@ -1,11 +1,16 @@
-import {BlockchainModule, Logger, Preparation} from "tank.bench-common";
+import {BlockchainModule, BuiltinBenchProfile, Logger, Preparation} from "tank.bench-common";
 import HayaModulePrepareStep from "./steps/HayaModulePrepareStep";
 import configTemplate from "./config/configSchema";
 import Constants from "./constants/Constants";
+import HayaModuleDefaultProfile from "./steps/HayaModuleDefaultProfile";
 
 export default class HayaModule extends BlockchainModule {
     createPreparationStep(commonConfig: any, moduleConfig: any, logger: Logger): Preparation {
         return new HayaModulePrepareStep(commonConfig, moduleConfig, logger);
+    }
+
+    getBuiltinProfiles(): BuiltinBenchProfile[] {
+        return [HayaModuleDefaultProfile.profileMeta];
     }
 
     getConfigSchema(): any {
