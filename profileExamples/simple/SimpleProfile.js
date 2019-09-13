b6af2b864640108012556c16d1127e2847a77669
Sergey Govyazin <SGV123@yandex.ru>
2019-09-13 11:56:01 +0300

    Edited the profile example; updated to 4.0.2 common

--- profileExamples/simple/SimpleProfile.js
+++ profileExamples/simple/SimpleProfile.js
@@ -1,10 +1,10 @@
 const SimpleBenchProfile = require("./SimpleBenchProfile");
-const configSchema = require("../../dist/config/configSchema");
+const configSchema = require("../../dist/config/configSchema").default;
 
 module.exports = {
     benchProfile: SimpleBenchProfile,
+    telemetryProfile: "takeFromBlockchainModuleDefaultProfile",
+    preparationProfile: "takeFromBlockchainModuleDefaultProfile",
     fileName: __filename,
-    preparationProfile: undefined,
-    telemetryProfile: undefined,
     configSchema: configSchema
 };
